7fcc2e4b31f15d3d72d76f8d706aba72748e6680
Timo Reusch <mail@timo-reusch.de>
2023-01-18 00:26:33 +0100

    Update authentication_vs_authorization.py

--- manim-animations/authentication_vs_authorization.py
+++ manim-animations/authentication_vs_authorization.py
@@ -3,6 +3,24 @@ from manim import *
 
 class AuthenticationVsAuthorization(Scene):
     def construct(self):
-        test = "a", "b", "c"
-        auth = BulletedList(test, height=2, width=2)
-        self.play(FadeIn(auth))
+        data = ["Identify the user \nthrough something he\n • knows \n • has \n • is",
+                "Is the user allowed to \ndo this?"]
+        table = Table(
+            [data],
+            col_labels=[Text("Authentication"), Text("Authorization")]
+        ).scale(0.6)
+
+        self.play(Create(table.get_horizontal_lines()), Create(table.get_vertical_lines()))
+        for i in table.get_labels():
+            self.play(Write(i))
+            self.wait(1)
+
+        for i in table.get_entries_without_labels():
+            self.play(FadeIn(i))
+            self.wait(1)
+
+        self.wait(1)
+
+        self.play(
+            *[FadeOut(mob)for mob in self.mobjects]
+        )
